265c19aba69db25a304a2a3fc7b0065b8ae13455
Zeotical <aceyems10@gmail.com>
2025-05-08 12:30:56 +0800

    info on top

--- main.cpp
+++ main.cpp
@@ -1,2 +1,15 @@
+/**********|**********|**********|
+Program: YOUR_FILENAME.cpp / YOUR_FILENAME.h 
+Course: Data Structures and Algorithms? oopds
+Trimester: 2410
+Name: Frank Carrano
+ID: 1071001234
+Lecture Section: TC101
+Tutorial Section: TT1L
+Email: abc123@yourmail.com
+Phone: 018-1234567
+**********|**********|**********/
+
+
 #include <iostream>
 using namespace std;

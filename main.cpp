cbe5e93a01ed3af0d78b88cbeb51fe7b950a0067
huichiy <yaphuichiy@gmail.com>
2025-05-12 13:56:55 +0800

    add class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /**********|**********|**********|
-Program: YOUR_FILENAME.cpp / YOUR_FILENAME.h 
+Program: YOUR_FILENAME.cpp / YOUR_FILENAME.h
 Course: Data Structures and Algorithms? oopds
 Trimester: 2410
 Name: Frank Carrano
@@ -10,7 +10,15 @@ Email: abc123@yourmail.com
 Phone: 018-1234567
 **********|**********|**********/
 
-
 #include <iostream>
 #include <vector>
 using namespace std;
+
+const int MaxWidth = 50;
+const int MaxHeight = 40;
+
+class BattleField
+{
+private:
+public:
+};

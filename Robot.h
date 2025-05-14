242eb2cc78c89072f1b16b9f480e08dd54f83519
Zeotical <aceyems10@gmail.com>
2025-05-14 16:04:37 +0800

    Robot random position pt 1

--- Robot.h
+++ Robot.h
@@ -1,7 +1,18 @@
 class Robot {};
+
+// move the below to robotcpp file
+
 // lives
 // position
 // type
 // name
 
 // basic robot functions etc
+
+// robotsPostionX = rand(0,50) //c++ doesnt really work like this for range
+//robotsPositionY = rand(0,40)
+
+//srand(time(0)); //each time program runs get a new random number
+
+//positionforx = rand() % 51 ; //(0 to 50)
+//positionfory = rand() % 41; // (0 to 40)

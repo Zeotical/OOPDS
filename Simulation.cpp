7a5d7c1904b2945f89569006f5b1b0b140407b76
huichiy <yaphuichiy@gmail.com>
2025-05-26 11:47:27 +0800

    Auto stash before merge of "battlefield" and "origin/battlefield"

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,2 +1,133 @@
 #include "Simulation.h"
+#include "Battlefield.h"
+#include "Robot.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+Simulation::Simulation() : currentTurn(0)
+{
+    // Initialize random seed
+    srand(time(0));
+}
+
+void Simulation::loadScenario(const string &filename)
+{
+    ifstream file(filename);
+    if (!file.is_open())
+    {
+        cerr << "Error: Could not open input file!" << endl;
+        exit(1);
+    }
+
+    string dummy;
+    file >> dummy >> dummy >> dummy >> battlefieldWidth >> battlefieldHeight;
+    file >> dummy >> maxTurns;
+    file >> dummy >> numRobots;
+
+    robots.clear();
+    for (int i = 0; i < numRobots; i++)
+    {
+        string type, name;
+        int x, y;
+        file >> type >> name >> x >> y;
+
+        if (type == "GenericRobot")
+        {
+            GenericRobot *robot = new GenericRobot(name);
+            if (x == -1 && y == -1)
+            { // Random position
+                auto pos = battlefield.getRandomEmptyPosition();
+                robot->setPosition(pos.first, pos.second);
+            }
+            else
+            {
+                robot->setPosition(x, y);
+            }
+            robots.push_back(robot);
+            battlefield.placeRobot(robot->getPositionX(), robot->getPositionY(), robot->getname());
+        }
+    }
+}
+
+void Simulation::run()
+{
+    ofstream logfile("simulation_log.txt");
+
+    for (currentTurn = 0; currentTurn < maxTurns; currentTurn++)
+    {
+        logfile << "=== Turn " << currentTurn << " ===" << endl;
+        cout << "=== Turn " << currentTurn << " ===" << endl;
+
+        // Process each robot's turn
+        for (Robot *robot : robots)
+        {
+            if (!robot->isAlive())
+                continue;
+
+            // Robot actions
+            robot->look(battlefield);
+            robot->think();
+            robot->move(battlefield);
+            robot->fire(battlefield);
+
+            // Log robot status
+            logfile << robot->getname() << " at ("
+                    << robot->getPositionX() << ","
+                    << robot->getPositionY() << ")" << endl;
+        }
+
+        // Print battlefield state
+        battlefield.printBattlefield();
+
+        // Check for game over condition
+        if (checkGameOver())
+        {
+            logfile << "Game over! Winner: " << getWinner()->getname() << endl;
+            cout << "Game over! Winner: " << getWinner()->getname() << endl;
+            break;
+        }
+    }
+
+    logfile.close();
+}
+
+bool Simulation::checkGameOver()
+{
+    int aliveCount = 0;
+    Robot *lastAlive = nullptr;
+
+    for (Robot *robot : robots)
+    {
+        if (robot->isAlive())
+        {
+            aliveCount++;
+            lastAlive = robot;
+        }
+    }
+
+    return aliveCount <= 1;
+}
+
+Robot *Simulation::getWinner()
+{
+    for (Robot *robot : robots)
+    {
+        if (robot->isAlive())
+        {
+            return robot;
+        }
+    }
+    return nullptr;
+}
+
+void Simulation::cleanup()
+{
+    for (Robot *robot : robots)
+    {
+        delete robot;
+    }
+    robots.clear();
+}
--- Simulation.h
+++ Simulation.h
@@ -1,1 +1,32 @@
-//hi
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include "Battlefield.h"
+#include "Robot.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Simulation
+{
+private:
+    BattleField battlefield;
+    vector<Robot *> robots;
+    int currentTurn;
+    int maxTurns;
+    int numRobots;
+    int battlefieldWidth;
+    int battlefieldHeight;
+
+    bool checkGameOver();
+    Robot *getWinner();
+
+public:
+    Simulation();
+    void loadScenario(const string &filename);
+    void run();
+    void cleanup();
+};
+
+#endif

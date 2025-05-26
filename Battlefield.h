d8938b7606d37a1db059c68c0c5eab5195c99b2a
huichiy <yaphuichiy@gmail.com>
2025-05-26 16:32:58 +0800

    debug
    - add function
    - since txt file will input the dimension, delete width = 30, height = 20
    - include 2 time string, delete 1

--- Battlefield.h
+++ Battlefield.h
@@ -2,28 +2,33 @@
 #define BATTLEFIELD_H
 
 #include <iostream>
-#include<string>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
 
 class BattleField
 {
+private:
+    int width;
+    int height;
+    vector<vector<string>> field;
 
 public:
- int width = 30;  // col, x
-  int height = 20 ; // row, y
-
-    vector<vector<string>> field ;
-    
-
     BattleField();
+    BattleField(int w, int h);
+    void initialize(int w, int h);
     void printBattlefield();
-    void placeRobot(int x, int y, string robot);
+    void placeRobot(int x, int y, const string &robot);
     void clearPosition(int x, int y);
-    bool isOccupied(int x, int y);
-    bool isInside(int x, int y);
+    bool isOccupied(int x, int y) const;
+    bool isInside(int x, int y) const;
     pair<int, int> getRandomEmptyPosition();
+    void moveRobot(int oldX, int oldY, int newX, int newY);
+
+    // Getters
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+    string getCell(int x, int y) const;
 };
 
 #endif

9237cd98faf2bd4aa2c29ef5762e67e9d888e5d9
Zeotical <aceyems10@gmail.com>
2025-06-01 15:45:09 +0800

    Name change

--- Simulation.cpp
+++ Simulation.cpp
@@ -6,6 +6,9 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <memory>
+
+
 
 Simulation::Simulation() : currentTurn(0)
 {
@@ -13,7 +16,7 @@ Simulation::Simulation() : currentTurn(0)
     srand(time(0));
 }
 
-void Simulation::loadScenario(const string &filename)
+void Simulation::loadScenario(const string &filename, BattleField &battle)
 {
     ifstream file(filename);
     if (!file.is_open())
@@ -34,8 +37,8 @@ void Simulation::loadScenario(const string &filename)
     file >> dummy >> numRobots;
 
     // Initialize battlefield with correct dimensions
-    battlefield.initialize(battlefieldWidth, battlefieldHeight);
-
+    battle.initialize(battlefieldWidth, battlefieldHeight);
+    cout << "⫘⫘⫘⫘⫘⫘✦✦ ROBOT BATTLE SIMULATION ✦✦⫘⫘⫘⫘⫘⫘" << endl; 
     cout << "Battlefield: " << battlefieldWidth << "x" << battlefieldHeight << endl;
     cout << "Max turns: " << maxTurns << endl;
     cout << "Number of robots: " << numRobots << endl;
@@ -49,68 +52,311 @@ void Simulation::loadScenario(const string &filename)
 
         if (type == "GenericRobot")
         {
-            GenericRobot *robot = new GenericRobot(name);
+            //GenericRobot *robot = new GenericRobot(name);
+               GenericRobot::robotObjects.emplace(name,name); // (key,obj)
+                 auto& robot =  GenericRobot::robotObjects.at(name); // doesn't need a defualt constructor as [] does
 
-            // Handle random position
+
+            //Handle random position
             if (xStr == "random" || yStr == "random")
             {
-                auto pos = battlefield.getRandomEmptyPosition();
-                robot->setPosition(pos.first, pos.second);
+                auto pos = battle.getRandomEmptyPosition();
+                //robot->setPosition(pos.first, pos.second);
+                robot.setPositionX(pos.first);
+                robot.setPositionY(pos.second);
                 cout << "Placed " << name << " at random position (" << pos.first << "," << pos.second << ")" << endl;
+                battle.placeRobot(pos.first,pos.second,robot.getname());
+
             }
-            else
+             else
             {
                 int x = stoi(xStr);
                 int y = stoi(yStr);
-                robot->setPosition(x, y);
-                cout << "Placed " << name << " at position (" << x << "," << y << ")" << endl;
+                robot.setPositionX(y);
+                robot.setPositionY(x);   
+
+                battle.placeRobot(robot.getPositionX(),robot.getPositionY(),robot.getname());
+
+                cout << "Placed " << name << " at position (" << y << "," << x << ")" << endl;
             }
 
-            robots.push_back(robot);
-            battlefield.placeRobot(robot->getPositionX(), robot->getPositionY(), robot->getname());
+            robots.push_back(&robot);
+            //battlefield.placeRobot(robot->getPositionX(), robot->getPositionY(), robot->getname());
         }
     }
+    battle.printBattlefield();
 
     file.close();
 }
 
-void Simulation::run()
+void Simulation::run(BattleField &battle)
 {
     ofstream logfile("simulation_log.txt");
-
-    for (currentTurn = 0; currentTurn < maxTurns; currentTurn++)
-    {
-        logfile << "=== Turn " << currentTurn << " ===" << endl;
-        cout << "=== Turn " << currentTurn << " ===" << endl;
-
+    currentTurn = -1;
+    GenericRobot* robot = nullptr;
+    int action =0;
+    //int random_spawn = rand() % maxTurns ;
+    //int random_spawn_times = 0; // make func if random_spawn is b/w so so maxturns then gen robot,name and push to vector shall i make a new inout file for this?
+     while ( currentTurn < maxTurns)
+    {    
+        //randomSpawn("randomSpawn.txt",battle,currentTurn);
         // Process each robot's turn
-        for (Robot *robot : robots)
-        {
-            if (!robot->isAlive())
-                continue;
+        for (int i = 0; i < robots.size();)
+        { 
+             robot = robots[i];
+            ++currentTurn;      
 
+            logfile << "=== Turn " << currentTurn << " ===" << endl;
+            cout << "=== Turn " << currentTurn << " (" << robot->name <<") ===" << endl;
+
+        //Handle dead Robots
+        if (!robot->isAlive()) {
+                for(int i =0 ; i< robots.size();i++) {
+                    if(robots[i] == robot) {
+                    robots.erase(robots.begin() + i);
+                    cout << "#### " << robot->name << " is dead #### (╥‸╥)" <<endl;  
+                    logfile << robot->name << " is dead" <<endl; 
+                    }
+                 continue; } }
+        //Handle cooldown for re-entering Robots
+        else if (robot->re_enteringRobots.size() > 0 && robot->cooldown == 1) {
+            cout << "|||| " << robot->name << " will respawn in it's next turn ||||" <<endl ;
+            logfile << robot->name << " will respawn in it's next turn" <<endl ;
+            --robot->cooldown ;
+            i++;
+            continue;
+        } 
+        //Handle re-entering Robots
+        else if(robot->re_enteringRobots.size() > 0  && robot->cooldown == 0)
+            {  
+                cout << robot->name << " is re-entering" << endl;
+                robot->re_enteringRobots.pop();
+                auto pos = battle.getRandomEmptyPosition();
+                //robot->setPosition(pos.first, pos.second);
+                robot->setPositionX(pos.first);
+                robot->setPositionY(pos.second);
+                cout << "Placed " << robot->name << " at random position (" << pos.first << "," << pos.second << ")" << endl;
+                battle.placeRobot(pos.first,pos.second,robot->name);
+                robot->choices.clear(); //robot re enters as a generic robot with 0 upgrades
+                robot->re_entryReset();
             // Robot actions
-            robot->look(battlefield);
-            robot->think();
-            robot->move(battlefield);
-            robot->fire(battlefield);
+            action = robot->think();
+
+            if (robot->upgrades == 0 || robot->choices.size() == 0) {
+                if (action == 1) {
+                    robot->look(battle);
+                    robot->move(battle);
+                }
+                else if (action == 2) {
+                    robot->look(battle);
+                    robot->fire(battle);
+                }
+                else {
+                    // optional fallback, maybe just look? or upgrades?
+                    robot->look(battle);
+                }
+            }
+
+            else {
+
+                int idx = rand() % robot->choices.size();
+                string choice = robot->choices[idx];
+
+                if (choice=="ScoutBot" && robot->scout_bot_uses < 3) {
+                    ++robot->scout_bot_uses ;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->ScoutBot(battle);
+                    if(robot->scout_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice== "TrackBot" && robot->track_bot_uses < 3) {
+                    ++robot->track_bot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->TrackBot(battle);
+                    if (!robot->trackedrobots.empty()){ //check if there are tracked robots first
+                        int i = rand() % robot->trackedrobots.size();
+                        robot->robottotrack = robot->trackedrobots[i] ;
+                        int posx = robot->robottotrack->getPositionX();
+                        int posy = robot->robottotrack->getPositionY();
+                        if (robot->think() == 2 && robot->robottotrack->isAlive()) {
+                        robot->enemyPos.push_back(make_pair(posy,posx));
+                        cout << robot->name << " will try to shoot the tracked robot " << robot->robottotrack->name << endl;
+                        robot->fire(battle);
+                    }
+                        else {
+                            //no logic is actually implemented for the below it's just random on whether it will move further or not
+                            cout << robot->name << " will try to move further from the tracked robot " << robot->robottotrack->name << endl;
+                            robot->look(battle);
+                            robot->move(battle);
+                        }
+
+                    }
+                    if(robot->track_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice=="HideBot") {} //TBD
+                else if (choice=="JumpBot" && robot->jump_bot_uses < 3) {
+                    ++robot->jump_bot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->JumpBot(battle);
+                    if(robot->jump_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice=="LongShotBot" && robot->long_shot_bot_uses < 3) {
+                    ++robot->long_shot_bot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->LongShotBot(battle);
+                    if(robot->long_shot_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice=="SemiAutoBot" && robot->semi_auto_bot_uses < 3) {
+                    ++robot->semi_auto_bot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->SemiAutoBot(battle);
+                    if(robot->semi_auto_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice=="ThirtyShotBot" && robot->thirty_shot_uses < 3) {
+                    ++robot->thirty_shot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->ThirtyShotBot();
+                    if(robot->thirty_shot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+               }
+           }
 
             // Log robot status
             logfile << robot->getname() << " at ("
                     << robot->getPositionX() << ","
                     << robot->getPositionY() << ")" << endl;
-        }
 
+            }
+        //Handle still alive robots
+        else if (robot->lives == 3 || (robot->lives <= 3 && robot->re_enteringRobots.size() == 0)) {
+            // Robot actions
+            action = robot->think();
+            if (robot->upgrades == 0 || robot->choices.size() == 0) {
+                if (action == 1) {
+                    robot->look(battle);
+                    robot->move(battle);
+                }
+                else if (action == 2) {
+                    robot->look(battle);
+                    robot->fire(battle);
+                }
+                else {
+                    // optional fallback, maybe just look? or upgrades?
+                    robot->look(battle);
+                }
+            }
+
+            else {
+
+                int idx = rand() % robot->choices.size();
+                string choice = robot->choices[idx];
+
+                if (choice=="ScoutBot" && robot->scout_bot_uses < 3) {
+                    ++robot->scout_bot_uses ;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->ScoutBot(battle);
+                    if(robot->scout_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice== "TrackBot" && robot->track_bot_uses < 3) {
+                    ++robot->track_bot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->TrackBot(battle);
+                    if (!robot->trackedrobots.empty()){ //check if there are tracked robots first
+                        int i = rand() % robot->trackedrobots.size();
+                        robot->robottotrack = robot->trackedrobots[i] ;
+                        int posx = robot->robottotrack->getPositionX();
+                        int posy = robot->robottotrack->getPositionY();
+                        if (robot->think() == 2 && robot->robottotrack->isAlive()) {
+                        robot->enemyPos.push_back(make_pair(posy,posx));
+                        cout << robot->name << "will try to shoot the tracked robot " << robot->robottotrack->name << endl;
+                        robot->fire(battle);
+                    }
+                        else {
+                            //no logic is actually implemented for the below it's just random on whether it will move further or not
+                            cout << robot->name << "will try to move further from the tracked robot " << robot->robottotrack->name << endl;
+                            robot->look(battle);
+                            robot->move(battle);
+                        }
+
+                    }
+                    if(robot->track_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice=="HideBot") {} //TBD
+                else if (choice=="JumpBot" && robot->jump_bot_uses < 3) {
+                    ++robot->jump_bot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->JumpBot(battle);
+                    if(robot->jump_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice=="LongShotBot" && robot->long_shot_bot_uses < 3) {
+                    ++robot->long_shot_bot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->LongShotBot(battle);
+                    if(robot->long_shot_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice=="SemiAutoBot" && robot->semi_auto_bot_uses < 3) {
+                    ++robot->semi_auto_bot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->SemiAutoBot(battle);
+                    if(robot->semi_auto_bot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+                }
+                else if (choice=="ThirtyShotBot" && robot->thirty_shot_uses < 3) {
+                    ++robot->thirty_shot_uses;
+                    cout << robot->name <<" is using " << choice << " upgrade" << endl;
+                    robot->ThirtyShotBot();
+                    if(robot->thirty_shot_uses == 3) {
+                        robot->choices.erase(robot->choices.begin() + idx);
+                    }
+               }
+           }
+
+            // Log robot status
+            logfile << robot->getname() << " at ("
+                    << robot->getPositionX() << ","
+                    << robot->getPositionY() << ")" << endl;
+
+            }
+            i++;
+            battle.printBattlefield();
+            randomSpawn("randomSpawn.txt",battle,currentTurn);
+
+        }
+            
         // Print battlefield state
-        battlefield.printBattlefield();
+        // battle.printBattlefield();
 
         // Check for game over condition
         if (checkGameOver())
         {
             logfile << "Game over! Winner: " << getWinner()->getname() << endl;
-            cout << "Game over! Winner: " << getWinner()->getname() << endl;
+            cout << "Game over! Winner: " << getWinner()->getname()  <<" ୧(๑•̀ヮ•́)૭ LET'S GO!" << endl;
             break;
         }
+        else if (currentTurn >= maxTurns){
+            cout << "No one won, game time has run out " << "( ˶°ㅁ°) !!" << endl;
+            break; //needed? if turn over it will just break already..
+        }
+
     }
 
     logfile.close();
@@ -121,9 +367,9 @@ bool Simulation::checkGameOver()
     int aliveCount = 0;
     Robot *lastAlive = nullptr;
 
-    for (Robot *robot : robots)
+    for (GenericRobot* robot : robots)
     {
-        if (robot->isAlive())
+         if (robot->isAlive())
         {
             aliveCount++;
             lastAlive = robot;
@@ -135,21 +381,72 @@ bool Simulation::checkGameOver()
 
 Robot *Simulation::getWinner()
 {
-    for (Robot *robot : robots)
+    for (GenericRobot* robot : robots)
     {
         if (robot->isAlive())
         {
             return robot;
-        }
+            }
     }
     return nullptr;
 }
 
 void Simulation::cleanup()
 {
-    for (Robot *robot : robots)
-    {
-        delete robot;
-    }
+    
     robots.clear();
+}
+
+void Simulation::randomSpawn(const string &filename,BattleField &battle, int Turn){
+string dummy, type, name, xStr, yStr;
+int numOfRobots;
+int entry_turn;
+ifstream file(filename);
+    if (!file.is_open())
+    {
+        cerr << "Error: Could not open input file: " << filename << endl;
+        exit(1);
+    }
+file >> dummy >> numOfRobots;
+for (int i =0; i < numOfRobots; i++) {
+file >> entry_turn >> type >> name >> xStr >> yStr ;
+if (entry_turn == Turn) {
+
+//15 GenericRobot cat random random  
+     if (type == "GenericRobot")
+        {
+            //GenericRobot *robot = new GenericRobot(name);
+               GenericRobot::robotObjects.emplace(name,name); // (key,obj)
+                 auto& robot =  GenericRobot::robotObjects.at(name); // doesn't need a defualt constructor as [] does
+
+
+            //Handle random position
+            if (xStr == "random" || yStr == "random")
+            {
+                auto pos = battle.getRandomEmptyPosition();
+                //robot->setPosition(pos.first, pos.second);
+                robot.setPositionX(pos.first);
+                robot.setPositionY(pos.second);
+                battle.placeRobot(pos.first,pos.second,robot.getname());
+
+                cout << "New Robot entering ~~<" << name << ">~~" << endl ;
+                cout << "Placed " << name << " at random position (" << pos.first << "," << pos.second << ")" << endl;
+            }
+             else
+            {
+                int x = stoi(xStr);
+                int y = stoi(yStr);
+                robot.setPositionX(y);
+                robot.setPositionY(x);   
+
+                battle.placeRobot(robot.getPositionX(),robot.getPositionY(),robot.getname());
+                cout << "New Robot entering <" << name << ">" << endl ;
+                cout << "Placed " << name << " at position (" << y << "," << x << ")" << endl;
+            }
+
+            robots.push_back(&robot);
+        }
+} //if loop
+}//for loop
+
 }
